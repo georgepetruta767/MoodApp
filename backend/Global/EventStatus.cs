2c9cf647374786a39350d12ca225fbff59445a75
georgepetruta767 <72539722+georgepetruta767@users.noreply.github.com>
2022-02-13 19:45:58 +0200

    edit/delete person + selected dates

---
+++ backend/Global/EventStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Global
+{
+    public enum EventStatus
+    {
+        Incoming,
+        InProgress,
+        Finished
+    }
+}
