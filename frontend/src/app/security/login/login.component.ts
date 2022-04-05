aac60bffdb93f5d2222899cb7656185a317f1c03
georgepetruta767 <72539722+georgepetruta767@users.noreply.github.com>
2022-04-05 10:01:53 +0300

    wip

--- frontend/src/app/security/login/login.component.ts
+++ frontend/src/app/security/login/login.component.ts
@@ -20,6 +20,18 @@ export class LoginComponent implements OnInit {
     this.setupForm();
   }
 
+  public googleSignIn() {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+  }
+/*
+  public googleLogIn() {
+
+  }*/
+
   private setupForm() {
     this.form = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.pattern("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")]),
--- frontend/src/app/security/security.module.ts
+++ frontend/src/app/security/security.module.ts
@@ -42,7 +42,6 @@ const routes: Routes = [
     AngularFireAuthModule
   ],
   providers: [
-    AngularFirestoreModule
   ]
 })
 export class SecurityModule { }
